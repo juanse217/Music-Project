f80474ebd9fc278d8ba7026586fcd691d4d3e638
juanse217 <juansebas08004@gmail.com>
2026-01-02 00:15:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##
+
+## MUSIC APP
+
+In this project, we'll focus on showing the use of DS in software development. The focus of this project is to create a music app with playlists, music and users. 
+
---
+++ src/main/java/com/sebastian/Main.java
@@ -0,0 +1,9 @@
+package com.sebastian;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //TODO: In this project, we'll practice some previous topics: Tests, Logging, exceptions and streams. 
+    }
+}
